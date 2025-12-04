b0a06700aec5dcd1216d06869f00515046fca57e
chaechae10 <chayeon0303@naver.com>
2025-12-04 13:40:14 +0900

    [feat] add yfinance api

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from app.routers import report_router
 from app.routers import dashboard_router
 import uvicorn
 from app.routers import analytics_router
+from app.routers import financial_router
 from app.routers import admin
 import os
 from dotenv import load_dotenv
@@ -31,6 +32,7 @@ app.include_router(report_router.router)
 app.include_router(dashboard_router.router)
 app.include_router(analytics_router.router)
 app.include_router(admin.router)
+app.include_router(financial_router.router)
 
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
